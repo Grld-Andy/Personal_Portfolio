ea48a8d9d32ef5364e3c2e1bac6b4c8a32eb703a
Andy Ansong <andyansong82@gmail.com>
2025-02-13 01:58:12 +0000

    chores group related skills

--- src/constants/skills.ts
+++ src/constants/skills.ts
@@ -1,116 +1,47 @@
 import Skill from "../types/Skill";
 
 export const skills: Array<Skill> = [
-  {
-    name: "JavaScript",
-    image: "/public/images/icons/javascript-original.svg",
-  },
-  {
-    name: "TypeScript",
-    image: "/public/images/icons/typescript-original.svg",
-  },
-  {
-    name: "React",
-    image: "/public/images/icons/react-original.svg",
-  },
-  {
-    name: "NodeJS",
-    image: "/public/images/icons/nodejs-original.svg",
-  },
-  {
-    name: "Python",
-    image: "/public/images/icons/python-original.svg",
-  },
-  {
-    name: "Django",
-    image: "/public/images/icons/django-plain.svg",
-  },
-  {
-    name: "Redis",
-    image: "/public/images/icons/redis-original.svg",
-  },
-  {
-    name: "PostgreSQL",
-    image: "/public/images/icons/postgresql-original.svg",
-  },
-  {
-    name: "MonogoDB",
-    image: "/public/images/icons/mongodb-original.svg",
-  },
-  {
-    name: "Git",
-    image: "/public/images/icons/git-original.svg",
-  },
-  {
-    name: "GitHub",
-    image: "/public/images/icons/github-original.svg",
-  },
-  {
-    name: "CSS3",
-    image: "/public/images/icons/css3-original.svg",
-  },
-  {
-    name: "HTML5",
-    image: "/public/images/icons/html5-original.svg",
-  },
-  {
-    name: "Docker",
-    image: "/public/images/icons/docker-original.svg",
-  },
-  {
-    name: "ElectronJS",
-    image: "/public/images/icons/electron-original.svg",
-  },
-  {
-    name: "Jest",
-    image: "/public/images/icons/jest-plain.svg",
-  },
-  {
-    name: "Playwright",
-    image: "/public/images/icons/playwright-original.svg",
-  },
-  {
-    name: "Nginx",
-    image: "/public/images/icons/nginx-original.svg",
-  },
-  {
-    name: "MySql",
-    image: "/public/images/icons/mysql-original.svg",
-  },
-  {
-    name: "Postman",
-    image: "/public/images/icons/postman-original.svg",
-  },
-  {
-    name: "Swagger",
-    image: "/public/images/icons/swagger-original.svg",
-  },
-  {
-    name: "Redux",
-    image: "/public/images/icons/redux-original.svg",
-  },
-  {
-    name: "Socker-IO",
-    image: "/public/images/icons/socketio-original.svg",
-  },
-  {
-    name: "Supabase",
-    image: "/public/images/icons/supabase-original.svg",
-  },
-  {
-    name: "RabbitMQ",
-    image: "/public/images/icons/rabbitmq-original.svg",
-  },
-  {
-    name: "NestJS",
-    image: "/public/images/icons/nestjs-original.svg",
-  },
-  {
-    name: "NextJS",
-    image: "/public/images/icons/nextjs-original.svg",
-  },
-  {
-    name: "ExpressJS",
-    image: "/public/images/icons/express-original.svg",
-  },
+  // Frontend Technologies
+  { name: "JavaScript", image: "/public/images/icons/javascript-original.svg" },
+  { name: "TypeScript", image: "/public/images/icons/typescript-original.svg" },
+  { name: "React", image: "/public/images/icons/react-original.svg" },
+  { name: "Redux", image: "/public/images/icons/redux-original.svg" },
+  { name: "NextJS", image: "/public/images/icons/nextjs-original.svg" },
+  { name: "ElectronJS", image: "/public/images/icons/electron-original.svg" },
+  { name: "HTML5", image: "/public/images/icons/html5-original.svg" },
+  { name: "CSS3", image: "/public/images/icons/css3-original.svg" },
+
+  // Backend Technologies
+  { name: "NodeJS", image: "/public/images/icons/nodejs-original.svg" },
+  { name: "ExpressJS", image: "/public/images/icons/express-original.svg" },
+  { name: "NestJS", image: "/public/images/icons/nestjs-original.svg" },
+  { name: "Django", image: "/public/images/icons/django-plain.svg" },
+  { name: "Python", image: "/public/images/icons/python-original.svg" },
+  { name: "Socket.IO", image: "/public/images/icons/socketio-original.svg" },
+  { name: "RabbitMQ", image: "/public/images/icons/rabbitmq-original.svg" },
+
+  // Databases
+  { name: "PostgreSQL", image: "/public/images/icons/postgresql-original.svg" },
+  { name: "MySQL", image: "/public/images/icons/mysql-original.svg" },
+  { name: "MongoDB", image: "/public/images/icons/mongodb-original.svg" },
+  { name: "Redis", image: "/public/images/icons/redis-original.svg" },
+  { name: "Supabase", image: "/public/images/icons/supabase-original.svg" },
+
+  // DevOps & Tools
+  { name: "Docker", image: "/public/images/icons/docker-original.svg" },
+  { name: "Nginx", image: "/public/images/icons/nginx-original.svg" },
+  { name: "Git", image: "/public/images/icons/git-original.svg" },
+  { name: "GitHub", image: "/public/images/icons/github-original.svg" },
+  { name: "Postman", image: "/public/images/icons/postman-original.svg" },
+  { name: "Swagger", image: "/public/images/icons/swagger-original.svg" },
+
+  // Testing
+  { name: "Jest", image: "/public/images/icons/jest-plain.svg" },
+  { name: "Playwright", image: "/public/images/icons/playwright-original.svg" },
+
+  // Additional Skills I Added
+  { name: "GraphQL", image: "/public/images/icons/graphql-original.svg" }, // Modern API query language
+  { name: "Prisma", image: "/public/images/icons/prisma-original.svg" }, // ORM for Node.js
+  { name: "Terraform", image: "/public/images/icons/terraform-original.svg" }, // Infrastructure as Code
+  { name: "Kubernetes", image: "/public/images/icons/kubernetes-original.svg" }, // Container orchestration
 ];
