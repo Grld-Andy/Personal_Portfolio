71759f5433179de1eb3a1ca70ea3fd6dc58a82c7
Andy Ansong <andyansong82@gmail.com>
2025-02-27 02:14:24 +0000

    add description to experience

--- src/constants/experience.ts
+++ src/constants/experience.ts
@@ -5,7 +5,8 @@ const experiences: Array<Experience> = [
     name: "University of Mines and Technology",
     position: "Bachelors of Computer Science and Engineering",
     location: "Ghana, Western Region, Tarkwa",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima laudantium veniam repudiandae id iure quidem sit nostrum adipisci, ipsum ea?",
+    description:
+      "Pursuing a Bachelor's degree in Computer Science and Engineering, gaining expertise in software development, data structures, algorithms, and system architecture. Engaged in hands-on projects and research to solve real-world computing problems.",
     date: "15th January, 2020 - 20th November, 2025",
     logo: "/images/experiences/umat-logo.png",
   },
@@ -13,7 +14,8 @@ const experiences: Array<Experience> = [
     name: "Persol Systems Limited",
     position: ".NET Intern",
     location: "Ghana, Greater Accra Region, Airport Residential",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima laudantium veniam repudiandae id iure quidem sit nostrum adipisci, ipsum ea?",
+    description:
+      "Worked on enterprise software solutions using .NET technologies. Assisted in developing, testing, and optimizing applications while collaborating with senior engineers to improve system performance and maintainability.",
     date: "15th September, 2023 - 20th December, 2023",
     logo: "/images/experiences/logo-persol-systems-limited.png",
   },
@@ -21,7 +23,8 @@ const experiences: Array<Experience> = [
     name: "Amalitech IT Services",
     position: "Backend Engineering Intern",
     location: "Ghana, Greater Accra Region, Tema Highway",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima laudantium veniam repudiandae id iure quidem sit nostrum adipisci, ipsum ea?",
+    description:
+      "Gained hands-on experience in backend development, focusing on building scalable APIs and database management. Worked with technologies like Node.js, Express, and PostgreSQL to optimize backend workflows.",
     date: "15th September, 2024 - 20th October, 2024",
     logo: "/images/experiences/amalitech-logo.jpg",
   },
@@ -29,7 +32,8 @@ const experiences: Array<Experience> = [
     name: "Scynett Services",
     position: "Backend Engineering Intern",
     location: "Remote",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima laudantium veniam repudiandae id iure quidem sit nostrum adipisci, ipsum ea?",
+    description:
+      "Developed and optimized backend services using C# and .NET. Built RESTful APIs, implemented database optimizations, and improved system performance by integrating caching mechanisms. Collaborated with a remote team to deliver scalable and maintainable solutions.",
     date: "22nd October, 2024 - 20th December, 2024",
     logo: "/images/experiences/scynett-logo.png",
   },
