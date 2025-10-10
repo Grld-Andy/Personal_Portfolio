430acc7074b9f9b3fdab873e94c8f975e89c94ad
Andy Ansong <andyansong82@gmail.com>
2025-10-10 10:05:52 +0000

    add project

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -2,10 +2,25 @@ import Project from "../types/Project";
 
 const projects: Array<Project> = [
   {
+    id: 9,
+    name: "Lymfz Engineering Limited Website",
+    description:
+      "Corporate website built with React and TypeScript to showcase company services.",
+    image: "/images/projects/lymfz_engineering.png",
+    website: "https://www.lymfzengineeringltd.com/",
+    techStack: [
+      "/images/icons/react-original.svg",
+      "/images/icons/framermotion-original.svg",
+      "/images/icons/css3-original.svg",
+      "/images/icons/typescript-original.svg",
+    ],
+    tag: "Work",
+  },
+  {
     id: 1,
     name: "LoanIt",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, fugiat.",
+      "Loan management app for tracking borrowed amounts and repayments using Django.",
     image: "/images/projects/loan_manage.png",
     github: "https://github.com/Grld-Andy/LoanIt",
     techStack: [
@@ -17,57 +32,10 @@ const projects: Array<Project> = [
     tag: "School",
   },
   {
-    id: 2,
-    name: "BookLet",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, fugiat.",
-    image: "/images/projects/book_shop.png",
-    github: "https://github.com/Grld-Andy/BookLet",
-    techStack: [
-      "/images/icons/html5-original.svg",
-      "/images/icons/css3-original.svg",
-      "/images/icons/python-original.svg",
-      "/images/icons/django-plain.svg",
-    ],
-    tag: "Personal",
-  },
-  {
-    id: 3,
-    name: "Game Showcase Website",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, fugiat.",
-    image: "/images/projects/hero_image.png",
-    website: "https://game-showcase-website-rho.vercel.app/",
-    github: "https://github.com/Grld-Andy/Game-Showcase-Website",
-    techStack: [
-      "/images/icons/react-original.svg",
-      "/images/icons/javascript-original.svg",
-      "/images/icons/css3-original.svg",
-    ],
-    tag: "Personal",
-  },
-  {
-    id: 4,
-    name: "Cross Platform Music App",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, fugiat.",
-    image: "/images/projects/Tune8.png",
-    website: "https://tune8.vercel.app/",
-    github: "https://github.com/Grld-Andy/Tune8",
-    techStack: [
-      "/images/icons/react-original.svg",
-      "/images/icons/javascript-original.svg",
-      "/images/icons/css3-original.svg",
-      "/images/icons/nodejs-original.svg",
-      "/images/icons/electron-original.svg",
-    ],
-    tag: "Personal",
-  },
-  {
     id: 5,
     name: "AmaliProject",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, fugiat.",
+      "Team project management tool built with React, Node.js, Redis, and PostgreSQL.",
     image: "/images/projects/project_showcase.png",
     website: "https://amali-project.vercel.app/",
     github: "https://github.com/Grld-Andy/Intern-Final-Project",
@@ -81,17 +49,34 @@ const projects: Array<Project> = [
     tag: "Work",
   },
   {
-    id: 6,
-    name: "Projects for fun",
+    id: 2,
+    name: "BookLet",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, fugiat.",
-    image: "/images/projects/canvas1.png",
-    website: "https://grld-andy.github.io/Canvas_demos/",
-    github: "https://github.com/Grld-Andy/Canvas_demos",
+      "Online bookstore built with Django, featuring user accounts, cart, and order tracking.",
+    image: "/images/projects/book_shop.png",
+    github: "https://github.com/Grld-Andy/BookLet",
     techStack: [
       "/images/icons/html5-original.svg",
       "/images/icons/css3-original.svg",
+      "/images/icons/python-original.svg",
+      "/images/icons/django-plain.svg",
+    ],
+    tag: "Personal",
+  },
+  {
+    id: 4,
+    name: "Cross Platform Music App",
+    description:
+      "Music player app built with React and Electron, supporting playlists and offline playback.",
+    image: "/images/projects/Tune8.png",
+    website: "https://tune8.vercel.app/",
+    github: "https://github.com/Grld-Andy/Tune8",
+    techStack: [
+      "/images/icons/react-original.svg",
       "/images/icons/javascript-original.svg",
+      "/images/icons/electron-original.svg",
+      "/images/icons/css3-original.svg",
+      "/images/icons/nodejs-original.svg",
     ],
     tag: "Personal",
   },
@@ -99,7 +84,7 @@ const projects: Array<Project> = [
     id: 7,
     name: "Blog App",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, fugiat.",
+      "React and TypeScript blog app for creating, editing, and reading posts.",
     image: "/images/projects/blog_app.png",
     website: "https://blog-app-puce-alpha.vercel.app/",
     github: "https://github.com/Grld-Andy/Blog_app",
@@ -114,12 +99,42 @@ const projects: Array<Project> = [
     id: 8,
     name: "Online Markdown Preview Website",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, fugiat.",
+      "Real-time Markdown previewer built with React and styled for GitHub syntax.",
     image: "/images/projects/markdown_app.png",
     website: "https://online-markdown-preview-vert.vercel.app/",
     github: "https://github.com/Grld-Andy/Online_Markdown_Preview",
     techStack: [
       "/images/icons/react-original.svg",
+      "/images/icons/css3-original.svg",
+      "/images/icons/javascript-original.svg",
+    ],
+    tag: "Personal",
+  },
+  {
+    id: 3,
+    name: "Game Showcase Website",
+    description:
+      "React website that displays video game details with real-time data from APIs.",
+    image: "/images/projects/hero_image.png",
+    website: "https://game-showcase-website-rho.vercel.app/",
+    github: "https://github.com/Grld-Andy/Game-Showcase-Website",
+    techStack: [
+      "/images/icons/react-original.svg",
+      "/images/icons/javascript-original.svg",
+      "/images/icons/css3-original.svg",
+    ],
+    tag: "Personal",
+  },
+  {
+    id: 6,
+    name: "Canvas Apps",
+    description:
+      "A collection of creative web experiments using HTML5 Canvas and JavaScript.",
+    image: "/images/projects/canvas1.png",
+    website: "https://grld-andy.github.io/Canvas_demos/",
+    github: "https://github.com/Grld-Andy/Canvas_demos",
+    techStack: [
+      "/images/icons/html5-original.svg",
       "/images/icons/css3-original.svg",
       "/images/icons/javascript-original.svg",
     ],
