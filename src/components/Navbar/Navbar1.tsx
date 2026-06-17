b808b7880b28e4de43aa003faf97621308f2c8cd
Andy Ansong <andyansong82@gmail.com>
2026-06-17 02:10:34 +0000

    Update Navbar1.tsx

--- src/components/Navbar/Navbar1.tsx
+++ src/components/Navbar/Navbar1.tsx
@@ -17,7 +17,7 @@ const Navbar1: React.FC = () => {
           <a href="#skills">Skills</a>
           <a href="#projects">Projects</a>
           <a href="#experience">Experience</a>
-          <a href="https://drive.google.com/file/d/17m9RLD-5kRgefu7fidiTvWhTF7Nh6dul/view?usp=drive_link">Resume</a>
+          <a href="https://drive.google.com/file/d/1iPgONbYGT3hHU_-mkP8D7X9rR3s9vMYs/view?usp=drive_link">Resume</a>
         </div>
       </div>
     </motion.nav>
